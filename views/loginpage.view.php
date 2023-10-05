0a84535592cd63110a34422894d4ee6d117907eb
Rick <143965784+RickBlaas@users.noreply.github.com>
2023-10-05 20:00:08 +0200

    first commit

---
+++ views/loginpage.view.php
@@ -0,0 +1,61 @@
+<?php
+//$fileName = 'personinfo.json';
+//
+//
+//if (isset($_POST['email']) && isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['password'])) {
+//    file_put_contents($fileName, json_encode($_POST));
+//}
+//
+//if(file_exists($fileName)) {
+//    $array= json_decode(file_get_contents($fileName), true);
+//    foreach($array as $value) {
+//        echo $value;
+//    }
+//}
+//
+//?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../style/registerstyle.css"/>
+    <meta charset="UTF-8">
+    <title>Login/Register</title>
+</head>
+
+<body>
+
+<div class="register"><p>Login</p>
+    <form method="post">
+        <div class="Userinfo">
+            <div class="input-field">
+                <label for="firstname">First name</label>
+                <input type="text" id="firstname" name="firstname" placeholder="Enter your first name" required>
+            </div>
+            <div class="input-field">
+                <label for="lastname">Last name</label>
+                <input type="text" id="lastname" name="lastname" placeholder="Enter your last name" required>
+            </div>
+            <div class="input-field">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" placeholder="Enter your username" required>
+            </div>
+            <div class="input-field">
+                <label for="email">Email adress</label>
+                <input type="email" id="email" name="email" placeholder="Enter your email" required>
+            </div>
+            <div class="input-field">
+                <label for="pwd">Password</label>
+                <input type="password" id="pwd" name="password" placeholder="Enter your password" required>
+            </div>
+
+            <button type="sumbit">Register</button>
+        </div>
+    </form>
+</div>
+
+</body>
+<footer>
+
+</footer>
+</html>
---
+++ views/register.view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../style/registerstyle.css"/>
+    <meta charset="UTF-8">
+    <title>Login/Register</title>
+</head>
+
+<body>
+<div class="registerform">
+    <div class="title">register<div>
+    <form action="#">
+        <div class="user-info">
+            <div class="input">
+                <
+            </div>
+        </div>
+    </form>
+</div>
+</body>
