355c513dd7da2890e11c797afa32dfeae0023c8a
Rick <rickblaas@live.nl>
2023-10-14 15:05:05 +0200

    made style changes

--- views/loginpage.view.php
+++ views/loginpage.view.php
@@ -25,48 +25,6 @@
 
     <form method="post">
         <div class="form-info">
-            <div class="title">Register</div>
-            <div class="input-field">
-                <label for="firstname">First name</label>
-                <input type="text" id="firstname" name="firstname" placeholder="Enter your first name" required>
-            </div>
-
-            <div class="input-field">
-                <label for="lastname">Last name</label>
-                <input type="text" id="lastname" name="lastname" placeholder="Enter your last name" required>
-            </div>
-
-            <div class="input-field">
-                <label for="username">Username</label>
-                <input type="text" id="username" name="username" placeholder="Enter your username" required>
-            </div>
-
-            <div class="input-field">
-                <label for="email">Email adress</label>
-                <input type="email" id="email" name="email" placeholder="Enter your email" required>
-            </div>
-
-            <div class="input-field">
-                <label for="pwd">Password</label>
-                <input type="password" id="pwd" name="password" placeholder="Enter your password" required>
-            </div>
-
-            <div class="input-field">
-                <label for="pwd">Confirm password</label>
-                <input type="password" id="pwd" name="confirm-password" placeholder="Confirm your password" required>
-            </div>
-
-        </div>
-        <div class="button">
-            <input type="submit" value="Register">
-        </div>
-    </form>
-</div>
-
-<div class="container">
-
-    <form method="post">
-        <div class="form-info">
             <div class="title">Login</div>
 
             <div class="input-field">
